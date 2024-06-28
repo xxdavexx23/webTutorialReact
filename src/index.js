73700a3d752157376d2042e0b2a08206d840778a
xxdavexx23 <70821018+xxdavexx23@users.noreply.github.com>
2024-06-28 21:21:18 +0000

    Up to Lesson 44

---
+++ src/index.js
@@ -0,0 +1,128 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+
+function App() {
+  return <div className="container">
+    <Header />
+    <Menu />
+    <Footer />
+    </div>;
+}
+
+
+
+function Header(){
+  // const style = {
+  //   color: 'red',
+  //   fontSize : "48px",
+  //   textTransform: "uppercase"
+  // };
+  const style = {};
+
+  return <header className="header">
+    <h1 style = {style}> 
+  Fast React Pizza Co.</h1>
+  </header>;
+}
+
+function getMenuItems(){
+  return pizzaData.map((pizza) => <Pizza 
+        name = {pizza.name} 
+        ingredients = {pizza.ingredients} 
+        photoName = {pizza.photoName} 
+        price = {pizza.price}
+        />);
+}
+
+function Menu(){
+  return(
+    <main className = 'menu'>
+      <h2>
+        Our Menu
+      </h2>
+        {getMenuItems()}
+    </main>
+  );
+}
+
+function Footer(){
+  const hour = new Date().getHours();
+  const openHour = 10;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour < closeHour;
+  console.log(isOpen);
+  // if(hour < openHour || hour >= closeHour){
+  //   alert("We are closed. Our business hours are from 12pm to 10");
+  // }
+  // else{
+  //   alert("We are open. Our business hours are from 12pm to 10pm");
+  // }
+
+  return <footer className="footer">
+    <p>{new Date().toLocaleDateString()} © 2021 Fast React Pizza Co. {isOpen ? "We are open" : "We are closed"}</p>
+  </footer>
+}
+
+
+
+function Pizza(props){
+  return <div className="pizza">
+    <img src= {props.photoName} alt={props.name} />
+    <h3>{props.name}</h3>
+    <p>{props.ingredients}</p>
+    <span>$ {props.price}</span>
+  </div>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<React.StrictMode>
+   <App />
+   </React.StrictMode>
+   );
